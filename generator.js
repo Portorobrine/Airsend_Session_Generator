834738679bc635fa8908bd4fd23d2c47f98fa168
Portorobrine <64094407+Portorobrine@users.noreply.github.com>
2023-01-03 16:31:53 +0100

    Add a tab for show devices

--- generator.js
+++ generator.js
@@ -22,4 +22,29 @@ async function generate() {
         document.getElementById('token').innerHTML = 'Error: ' + e ;
     }
     document.getElementById('token').innerHTML = token;
+    getDevice(token);
+}
+
+async function getDevice(token) {
+    const url = 'https://airsend.cloud/device';
+    const params = {
+        headers: {
+            'accept': 'application/json'
+        },
+        method: 'GET'
+    };
+    const URL = url + '?session=' + token;
+    try {
+        const response = await fetch(URL, params);
+        var devices = await response.json();
+    } catch (e) {
+        document.getElementById('device').innerHTML = 'Error: ' + e;
+    }
+    //create a tableau to display the devices
+    var out = '<table><tr><th>Device Name</th><th>Device ID</th></tr>';
+    for (var i = 0; i < devices.length; i++) {
+        out += '<tr><td>' + devices[i].name + '</td><td>' + devices[i].id + '</td></tr>';
+    }
+    out += '</table>';
+    document.getElementById('devices').innerHTML = out;
 }
